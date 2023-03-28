9e5a1f6c9de2d26ca6791faa97d03ea73720e7be
Flavio Dobler <flaviodobler@Flavios-MacBook-Air.local>
2023-03-28 16:23:07 -0300

    Desafio Finalizado

---
+++ DataHora.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let date = Date()
+
+func checkTime(_ data : Date) {
+    let calendar = Calendar.current
+    let hour = calendar.component(.hour, from: date)
+    
+    switch hour {
+    case 0..<12 :
+        print ("Bom dia!!")
+    case 12..<18 :
+        print ("Boa tade!!")
+    case 18..<24 :
+        print ("Boa noite!!")
+    default :
+        print ("Ainda está acordado?")
+    }
+}
+
+checkTime(date)
+
